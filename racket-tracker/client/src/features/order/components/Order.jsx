ddab026f79afda1f0c1ab2b4c4da76943ba85ab7
Alexander Smeulders <absmeulders@gmail.com>
2026-06-26 08:35:40 -0400

    styling order list item

--- racket-tracker/client/src/features/order/components/Order.jsx
+++ racket-tracker/client/src/features/order/components/Order.jsx
@@ -28,19 +28,44 @@ export function Order({order}) {
     }
   }
 
+  const getStatusClassName = () => {
+    const className = 'order-status ';
+    const now = new Date();
+    const due = new Date(order.due);
+    if (order.complete) {
+      return className + 'order-status--done';
+    } else if (now < due) {
+      return className + 'order-status--to-do';
+    } else {
+      return className + 'order-status--late';
+    }
+  }
+
+  const getStatusText = () => {
+    const now = new Date();
+    const due = new Date(order.due);
+    if (order.complete) {
+      return 'Done';
+    } else if (now < due) {
+      return 'To Do';
+    } else {
+      return 'Late';
+    }
+  }
+
   return (
     <div className='order-container'>
-      <div>{order?.user?.firstName} {order?.user?.lastName}</div>
-      <div>{order.complete ? "Done" : "To Do"}</div>
-      <div>{displayDate}</div>
-      <div>Racket: {order.racketBrand} {order.racketName}</div>
-      <div>Strings: {order.sameForCrosses ? 
+      <div className='order-info' >{order?.user?.firstName} {order?.user?.lastName}</div>
+      <div className={getStatusClassName()} >{getStatusText()}</div>
+      <div className='order-info' >{displayDate}</div>
+      <div className='order-info' >Racket: {order.racketBrand} {order.racketName}</div>
+      <div className='order-info' >Strings: {order.sameForCrosses ? 
         order?.jobDetails?.[0]?.stringBrand + ' ' + order?.jobDetails?.[0]?.stringName + ' @' + order?.jobDetails?.[0]?.tension + 'lbs'
         :
         '(M) ' + order?.jobDetails?.[0]?.stringBrand + ' ' + order?.jobDetails?.[0]?.stringName + ' @' + order?.jobDetails?.[0]?.tension + 'lbs ' +
         '(C) ' + order?.jobDetails?.[0]?.stringBrand + ' ' + order?.jobDetails?.[0]?.stringName + ' @' + order?.jobDetails?.[1]?.tension + 'lbs'
       }</div>
-      <div>${order.price} - {order.paid ? 'Paid' : "Unpaid"}</div>
+      <div className='order-info' >${order.price} - {order.paid ? 'Paid' : "Unpaid"}</div>
     </div>
   )
 }
