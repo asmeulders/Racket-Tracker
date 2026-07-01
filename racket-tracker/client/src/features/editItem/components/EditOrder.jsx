f4168dbe46a3616e211d644a9e52ed47b4f7bebf
asmeulders <alexs23@bu.edu>
2026-07-01 19:56:04 -0400

    working on order model update with labor cost

--- racket-tracker/client/src/features/editItem/components/EditOrder.jsx
+++ racket-tracker/client/src/features/editItem/components/EditOrder.jsx
@@ -22,6 +22,14 @@ export const EditOrder = ({ onEditItem, item }) => {
         <>
             <h1>Edit order {data.id}</h1>
             <OrderForm onDataCreated={onEditItem} order={item} rackets={data.rackets} strings={data.strings} users={data.users} />
+            <div>
+                <h3>Original Order</h3>
+                <div>Customer: {item.user.firstName} {item.user.lastName}</div>
+                <div>Racket: {item.racketBrand} {item.racketName}</div>
+                <div>Mains: {item.jobDetails[0].stringBrand} {item.jobDetails[0].stringName} @ {item.jobDetails[0].tension}lbs</div>
+                {!item.sameForCrosses && <div>Crosses: {item.jobDetails[1].stringBrand} {item.jobDetails[1].stringName} @ {item.jobDetails[1].tension}lbs</div>}
+                <div>Due: {item.due}</div>
+            </div>
         </>
     )
 }
